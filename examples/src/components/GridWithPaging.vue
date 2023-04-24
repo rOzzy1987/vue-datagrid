6172595ca75022fe9845980c7137edb84f944e75
rOzzy1987 <rozovits.mihaly@gmail.com>
2023-04-24 11:07:30 +0200

    paging examples updated

--- examples/src/components/GridWithPaging.vue
+++ examples/src/components/GridWithPaging.vue
@@ -1,25 +1,41 @@
 <template>
   <div>
-    <p class="block">Paging can be achievedin two ways: either specify the maximum number of items per page or load the content as you go</p>
-    <data-grid title="Paging with fixed dataset" :model-value="rows" v-model:columns="cols" 
-      :items-per-page="20" 
-      :is-items-per-page-editable="true"
-      :is-sortable="true"
-      paging-footer-text="page {0} of {1}" 
-      items-footer-text="{0} items"/>
-    <data-grid title="Using dynamic dataset" :model-value="showMoreRows" v-model:columns="cols" 
-      :is-sortable="true"
-      :paging-mode="GridPagingMode.ShowMore" @showMore="showMoreFunc"/>
-    <button class="button" @click.prevent="showMoreRows = []; showMoreFunc(null);">Reset</button>
-    <data-grid title="Using server side paging" :model-value="remoteRows" v-model:columns="remoteCols" 
-      :paging-mode="GridPagingMode.Remote" 
-      :items-per-page="20" 
-      :item-count="remoteRowsCache.length"
-      :is-items-per-page-editable="true"
-      :is-sortable="true"
-      paging-footer-text="page {0} of {1}"
-      items-footer-text="{0} items"
-      @paging="serverSidePaging" />
+    <p class="block">Paging can be achieved in three ways: either specify the maximum number of items per page or load the content as you go, or just display data from a server.</p>
+      
+    <div class="section">
+      <p class="block">The simplest is client side paging. You load the data, set the <span class="is-family-monospace">itemsPerPage</span> property and you're done</p>
+      <data-grid title="Paging with fixed dataset" :model-value="rows" v-model:columns="cols" 
+        :items-per-page="20" 
+        :is-items-per-page-editable="true"
+        :is-sortable="true"
+        paging-footer-text="page {0} of {1}" 
+        items-footer-text="{0} items"/>
+    </div>
+
+    <div class="section">
+      <p class="block">If you want to load data on demand but display everything loaded so far, just set the <span class="is-family-monospace">pagingMode</span> property to 
+        <span class="is-family-monospace">ShowMore</span> and create a handler for the <span class="is-family-monospace">@showMore</span> event</p>
+      <p class="block">Please not that sorting is disabled in this case</p>
+      <data-grid title="Using dynamic dataset" :model-value="showMoreRows" v-model:columns="cols" 
+        :is-sortable="true"
+        :paging-mode="GridPagingMode.ShowMore" @showMore="showMoreFunc"/>
+      <button class="button" @click.prevent="showMoreRows = []; showMoreFunc(null);">Reset</button>
+    </div>
+
+    <div class="section">
+      <p class="block">Using server side paging requires you to set <span class="is-family-monospace">pagingMode</span> property to 
+        <span class="is-family-monospace">Remote</span>, <span class="is-family-monospace">itemCount</span> property to the total item count and create a handler for the <span class="is-family-monospace">@paging</span> event</p>
+      <p class="block">Please not that sorting is disabled in this case</p>
+      <data-grid title="Using server side paging" :model-value="remoteRows" v-model:columns="remoteCols" 
+        :paging-mode="GridPagingMode.Remote" 
+        :items-per-page="20" 
+        :item-count="remoteRowsCache.length"
+        :is-items-per-page-editable="true"
+        :is-sortable="true"
+        paging-footer-text="page {0} of {1}"
+        items-footer-text="{0} items"
+        @paging="serverSidePaging" />
+    </div>
   </div>
 </template>
 
